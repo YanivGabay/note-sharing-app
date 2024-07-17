b86a8d9a034877c5d940ecdc9438dc1954d6df70
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-07-17 19:02:33 +0300

    basic file structure

---
+++ src/pages/Register.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input type="email" placeholder="Enter your email" />
+        <input type="password" placeholder="Create a password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
